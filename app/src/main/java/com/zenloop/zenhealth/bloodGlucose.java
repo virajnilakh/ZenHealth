854112f61ed668c683172f3e5429bf067ef31ead
virajnilakh <virajnilakh220@gmail.com>
2017-11-20 03:18:55 -0800

    Added button and layout to enter blood glucose manually

---
+++ app/src/main/java/com/zenloop/zenhealth/bloodGlucose.java
@@ -0,0 +1,13 @@
+package com.zenloop.zenhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bloodGlucose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_glucose);
+    }
+}
