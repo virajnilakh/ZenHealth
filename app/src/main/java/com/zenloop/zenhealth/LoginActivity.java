2fa7b2813fa6a5eeeae8b1600ce0c717e6bc268b
virajnilakh <viraj.nilakh@n3n.io>
2018-01-31 20:29:22 -0800

    Simple Login and Advanced graph

--- app/src/main/java/com/zenloop/zenhealth/LoginActivity.java
+++ app/src/main/java/com/zenloop/zenhealth/LoginActivity.java
@@ -3,6 +3,7 @@ package com.zenloop.zenhealth;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "viraj@gmail.com:12345", "bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -333,7 +334,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent myIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(myIntent);
+
+                //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
